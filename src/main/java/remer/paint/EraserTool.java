354df2d9a1e5cb88897911050cd7fab9c4624c49
shoshana remer <bellaremer@Bellas-MacBook-Air.local>
2025-04-02 19:02:52 -0400

    new classes

---
+++ src/main/java/remer/paint/EraserTool.java
@@ -0,0 +1,8 @@
+package remer.paint;
+
+public class EraserTool
+{
+    /**
+     * paint white with the X and Y coordinates
+     */
+}
---
+++ src/test/java/remer/paint/LineToolTest.java
@@ -0,0 +1,24 @@
+package remer.paint;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineToolTest {
+
+    @Test
+    void pressed() {
+    }
+
+    @Test
+    void dragged() {
+    }
+
+    @Test
+    void preview() {
+    }
+
+    @Test
+    void released() {
+    }
+}
